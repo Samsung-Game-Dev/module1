6bf070810cea7085c6667f3a782ebc460f1c0cc5
Anastasia Tarazevich <56476925+Anastasiyatarazevich@users.noreply.github.com>
2026-09-16 16:02:27 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String person = "\uD83E\uDDD9\u200D";
+        String person = "\uD83E\uDDD9";
+        String monster = "\uD83E\uDDDF";
+        
         int personLive = 3;
-
-        String monster = "\uD83E\uDDDF\u200D";
-        int sizeBoard = 3;
-        int personX = 1;
-        int personY = 3;
-
+        int sizeBoard = 5;
+        int personX;
+        int personY;
         int step = 0;
-        // \n, \t - спец символ
-        String gamingField = "+ —— + —— + —— +\n"
-                + "|    |    | \uD83C\uDFE0 |\n"
-                + "+ —— + —— + —— +\n"
-                + "|    | " + monster + " |    |\n"
-                + "+ —— + —— + —— +\n"
-                + "| " + person + " |    |    |\n"
-                + "+ —— + —— + —— +";
 
+        personX = 1 + sizeBoard / 2;
+        personY = 1 + sizeBoard / 2;
+        // \n, \t - спец символ
+        String gamingField = "+ —— + —— + —— + —— + —— +\n"
+                + "|    |    |    |    |    |\n"
+                + "+ —— + —— + —— + —— + —— +\n"
+                + "|    |    |    |    |    |\n"
+                + "+ —— + —— + —— + —— + —— +\n"
+                + "|    |    | " + monster + " |    |    |\n"
+                + "+ —— + —— + —— + —— + —— +\n"
+                + "|    |    |    |    |    |\n"
+                + "+ —— + —— + —— + —— + —— +\n"
+                + "| " + person + " |    |    |    |    |\n"
+                + "+ —— + —— + —— + —— + —— +";
+        
         System.out.println("Привет! Ты готов начать играть в игру? (Напиши: ДА или НЕТ)");
 
-        Scanner sc = new Scanner(System.in);
-        String answer = sc.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+
         System.out.println("Ваш ответ:\t" + answer);
-
-        System.out.println("Выбери сложность игры(от 1 до 5):");
-        int difficultGame = sc.nextInt();
-        System.out.println("Выбранная сложность:\t" + difficultGame);
-
     }
 }
